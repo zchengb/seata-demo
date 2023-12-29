555c340079980e702affb87d98c3952360db1035
zchengb <zxchengb@163.com>
2023-12-29 17:01:31 +0800

    feat: optimize response message.

--- order/src/main/java/cn/zchengb/order/configuration/GlobalControllerExceptionHandler.java
+++ order/src/main/java/cn/zchengb/order/configuration/GlobalControllerExceptionHandler.java
@@ -1,5 +1,7 @@
 package cn.zchengb.order.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,6 +15,9 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class GlobalControllerExceptionHandler {
+    private static final String ILLEGAL_ARGUMENT = "ILLEGAL_ARGUMENT";
+    private final ObjectMapper objectMapper;
+
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(INTERNAL_SERVER_ERROR)
     public ErrorResult handleRuntimeException(Exception e) {
@@ -24,13 +29,34 @@ public class GlobalControllerExceptionHandler {
     @ResponseStatus(BAD_REQUEST)
     public ErrorResult handleBadRequestException(IllegalArgumentException e) {
         log.error("handleBadRequestException: " + e.getMessage(), e);
-        return new ErrorResult("ILLEGAL_ARGUMENT", e.getMessage());
+        return new ErrorResult(ILLEGAL_ARGUMENT, e.getMessage());
     }
 
     @ExceptionHandler(NullPointerException.class)
     @ResponseStatus(BAD_REQUEST)
     public ErrorResult handleNullBadRequestException(NullPointerException e) {
         log.error("handleBadRequestException: " + e.getMessage(), e);
-        return new ErrorResult("ILLEGAL_ARGUMENT", e.getMessage());
+        return new ErrorResult(ILLEGAL_ARGUMENT, e.getMessage());
+    }
+
+    @ExceptionHandler(FeignException.class)
+    @ResponseStatus(BAD_REQUEST)
+    public ErrorResult handleFeignException(FeignException e) {
+        var errorMessage = extractErrorMessage(e);
+        return new ErrorResult(ILLEGAL_ARGUMENT, errorMessage);
+    }
+
+    private String extractErrorMessage(FeignException e) {
+        try {
+            if (e.responseBody().isPresent()) {
+                var responseBody = new String(e.responseBody().get().array());
+                var jsonNode = objectMapper.readTree(responseBody);
+                return jsonNode.path("message").asText();
+            }
+            return e.getMessage();
+        } catch (Exception error) {
+            log.error("Failed to extract error message from FeignException", error);
+            return error.getMessage();
+        }
     }
 }
